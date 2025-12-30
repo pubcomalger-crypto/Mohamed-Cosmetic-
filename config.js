62cd94a9114eb4bbc69b5e14c245ddd190926e4e
pubcomalger-crypto <pubcom.alger@gmail.com>
2025-12-30 12:40:06 +0100

    Mohamed Cosmetic
    Mohamed Cosmetic

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const profileConfig = {
     // Informations de base
 
 
-    name: "COCA",
-    bio: "SALAM",
-    profileImage: "https://i.ibb.co/NgKMhgMT/cocacola.jpg",
+    name: "Mohamed Cosmetic",
+    bio: "تعامل بحترام كل زبائن بيع بصدق والمصدقية",
+    profileImage: "https://scontent.falg7-6.fna.fbcdn.net/v/t39.30808-1/543384978_3967319893480749_2372044722443050960_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=102&ccb=1-7&_nc_sid=1d2534&_nc_ohc=JE4iDeoN8QwQ7kNvwGXu9RT&_nc_oc=AdmVzOJEDpILCJE1mlWWUWLH5OhptCltaVGTE2kbqlODdvkyq3_8egVUqCwyh-PNcWc&_nc_zt=24&_nc_ht=scontent.falg7-6.fna&_nc_gid=4LfDcNgrQ62i1MrKJfeoEg&oh=00_AfmDEFYq1X62WdgdMgUqtNKJLj8IMTlM2a_NZlGDIRSg8Q&oe=69597297",
     
 
 
@@ -15,17 +15,18 @@ const profileConfig = {
 
     // Réseaux sociaux
     socialLinks: {
-        facebook: "https://www.facebook.com/share/16dysjBQvt/?mibextid=wwXIfr",
-        instagram: "https://www.instagram.com/riyadmahrez26.7?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-        tiktok: "https://www.tiktok.com/@chebkhaled?is_from_webapp=1&sender_device=pc",
-        whatsapp: "+213540253369",
+        facebook: "https://www.facebook.com/share/1CETifinbV/",
+        facebook: "https://www.facebook.com/share/1BV2bfCb1u/",
+        instagram: "https://www.instagram.com/mohamed_cosmitic?utm_source=qr&igsh=cW5kbXQ5ZXlwNGJi",
+        tiktok: "https://www.tiktok.com/@mohamedcosmetic92?_r=1&_t=ZS-92cuCHjNtY4",
+        whatsapp: "+213770201979",
         telegram: "+213 562 33 14 08",
         linkedin: "daisyparkhouse",
         snapchat: "https://www.snapchat.com/@sheikh_tidiane?sender_web_id=792b2a4d-05d3-4e8f-a5e0-1aeb89eba722&device_type=desktop&is_copy_url=true",
         email: "pubcom.alger@gmail.com",
         website: "https://www.youtube.com/",
         maps: "https://share.google/T1z7NAFbftnLYNKZy",
-        phone: "+213658546209"
+        phone: "+213770201979"
     },
     
 
@@ -37,16 +38,17 @@ const profileConfig = {
     // Activation des icônes (mettre true pour afficher, false pour masquer)
     enabledIcons: {
         facebook: true,      // Afficher Facebook
+        facebook: true,      // Afficher Facebook
         instagram: true,     // Afficher Instagram
         tiktok: true,        // Afficher TikTok
         whatsapp: true,      // Afficher WhatsApp
-        telegram: true,      // Afficher Telegram
+        telegram: false,      // Afficher Telegram
         linkedin: false,     // Afficher LinkedIn (mettre true pour l'activer)
         website: false,       // Afficher Site Web
-        maps: true,          // Afficher Maps
+        maps: false,          // Afficher Maps
         phone: true,         // Afficher Téléphone
-        snapchat: true,      // Afficher Snapchat
-        email: true,         // Afficher Email
+        snapchat: false,      // Afficher Snapchat
+        email: false,         // Afficher Email
     },
     
 };
@@ -83,3 +85,4 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.profileConfig = profileConfig;
 }
+
